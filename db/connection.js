cf3baa3c548c264ddf484544061a2e637565e5ae
Gassama <gassma803@gmail.com>
2024-01-27 00:05:16 +0100

    on va commencer à travailler avec firebase,la première tentative a generé pas mal de dysfonctionnement

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
 
-const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const connectionString = process.env.MONGO_URI
 //connection à la base de données
-const connectDB = ()=>{
-  return  mongoose.connect(connectionString)
-}
+// const connectDB = ()=>{
+//   return  mongoose.connect(connectionString)
+// }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(connectionString);
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.error("MongoDB Connection Failed:", error.message);
+    process.exit(1); // Quitter l'application en cas d'échec de la connexion
+  }
+};
+
 
 module.exports = connectDB      
 
