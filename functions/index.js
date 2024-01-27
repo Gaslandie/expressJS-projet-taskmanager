603b0b564ebc73047b7aa271f17998345c6273df
Gassama <gassma803@gmail.com>
2024-01-27 03:08:14 +0100

    changement du nom de la fonction

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
 const functions = require("firebase-functions");
 const app = require("./app"); // Assurez-vous que ce chemin est correct
 
-exports.api = functions.https.onRequest(app);
+exports.app = functions.https.onRequest(app);
